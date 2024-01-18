88d423cc623ef03ea73cd5ccfcc61b5241758e88
TC Wang <wangtct@hotmail.com>
2024-01-18 15:43:19 +0800

    navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-2">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Chart.js</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                A7重劃區 2024 選舉結果分析
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">總統選舉</a>
+                </li>
+                <li><a class="dropdown-item" href="#">立委選舉</a></li>
+                <li><a class="dropdown-item" href="#">人口趨勢</a></li>
+              </ul>
+            </li>
+          </ul>
+
+          <a class="m-auto" aria-current="page" href="#">登入</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container-fluid">
+    <NavBar />
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
