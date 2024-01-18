4e747fe30f80b2a35846b5dabad8b649296bccb0
TC Wang <wangtct@hotmail.com>
2024-01-18 15:28:53 +0800

    bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/image-edge", "@vueuse/nuxt"],
+  css: ["~/assets/css/bootstrap.scss", "~/assets/css/my.css"],
+
+  plugins: [
+    {
+      src: "~/plugins/bootstrap.js",
+      mode: "client",
+    },
+  ],
   vite: {
     define: {
       "process.env.DEBUG": false,
